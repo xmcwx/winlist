841953a449299574958ade16571003584b2fe6a6
xmcwx <110288574+xmcwx@users.noreply.github.com>
2024-09-20 09:53:35 -1000

    Add files via upload

---
+++ 1.go
@@ -0,0 +1,287 @@
+package main
+
+import (
+	"archive/zip"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+// FileInfo represents file information
+type FileInfo struct {
+	Name     string `json:"name"`
+	Size     int64  `json:"size"`
+	Type     string `json:"type"`
+	Path     string `json:"path"`
+	SHA256   string `json:"sha256"`
+	Created  string `json:"created"`
+	Modified string `json:"modified"`
+	Accessed string `json:"accessed"`
+}
+
+// getSHA256 calculates the SHA256 hash of a file
+func getSHA256(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+// getFileTimestamps returns the created, modified, and accessed timestamps of a file
+func getFileTtimestamps(path string) (created, modified, accessed string, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", "", "", err
+	}
+	defer file.Close()
+
+	var winFileData syscall.Win32FileAttributeData
+	err = syscall.GetFileAttributesEx(syscall.StringToUTF16Ptr(path), syscall.GetFileExInfoStandard, (*byte)(unsafe.Pointer(&winFileData)))
+	if err != nil {
+		return "", "", "", err
+	}
+
+	created = time.Unix(0, winFileData.CreationTime.Nanoseconds()).UTC().Format(time.RFC3339)
+	modified = time.Unix(0, winFileData.LastWriteTime.Nanoseconds()).UTC().Format(time.RFC3339)
+	accessed = time.Unix(0, winFileData.LastAccessTime.Nanoseconds()).UTC().Format(time.RFC3339)
+
+	return created, modified, accessed, nil
+}
+
+// processFile processes a single file and returns its FileInfo
+func processFile(path string, logger *log.Logger) (*FileInfo, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		logger.Printf("[%s] [ERROR] Error accessing path %s: %v\n", time.Now().UTC().Format(time.RFC3339), path, err)
+		return nil, err
+	}
+
+	created, modified, accessed, err := getFileTtimestamps(path)
+	if err != nil {
+		logger.Printf("[%s] [ERROR] Error getting file times for %s: %v\n", time.Now().UTC().Format(time.RFC3339), path, err)
+		return nil, err
+	}
+
+	sha256Sum, err := getSHA256(path)
+	if err != nil {
+		logger.Printf("[%s] [ERROR] Error calculating hash for %s: %v\n", time.Now().UTC().Format(time.RFC3339), path, err)
+		return nil, err
+	}
+
+	return &FileInfo{
+		Name:     fileInfo.Name(),
+		Size:     fileInfo.Size(),
+		Type:     strings.ToUpper(filepath.Ext(path)),
+		Path:     path,
+		SHA256:   sha256Sum,
+		Created:  created,
+		Modified: modified,
+		Accessed: accessed,
+	}, nil
+}
+
+// addFileToZip adds a file to a ZIP archive.
+func addFileToZip(zipWriter *zip.Writer, filename string) error {
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	header.Name = filepath.Base(filename)
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
+
+// createSelfDeleteScript creates a temporary PowerShell script to delete the executable and itself
+func createSelfDeleteScript(exePath string) (string, error) {
+	deleteScript := fmt.Sprintf(`Start-Sleep -s 3; Remove-Item -Path "%s" -Force; Remove-Item -Path "$($MyInvocation.MyCommand.Path)" -Force`, exePath)
+
+	scriptPath := filepath.Join(os.TempDir(), "self_delete.ps1")
+	err := os.WriteFile(scriptPath, []byte(deleteScript), 0666)
+	if err != nil {
+		return "", err
+	}
+
+	return scriptPath, nil
+}
+
+// worker function to process files from the queue
+func worker(fileQueue <-chan string, results chan<- *FileInfo, logger *log.Logger, wg *sync.WaitGroup) {
+	for path := range fileQueue {
+		fileInfo, err := processFile(path, logger)
+		if err == nil && fileInfo != nil {
+			results <- fileInfo
+		}
+		wg.Done()
+	}
+}
+
+func main() {
+	startTime := time.Now().UTC()
+	startTimeStr := startTime.Format(time.RFC3339)
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal("Could not get hostname:", err)
+	}
+
+	logFileName := hostname + ".log"
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Could not create log file:", err)
+	}
+	defer logFile.Close()
+
+	logger := log.New(logFile, "", 0) // properly initialized Logger
+
+	jsonlFileName := hostname + ".jsonl"
+	jsonlFile, err := os.Create(jsonlFileName)
+	if err != nil {
+		logger.Printf("[%s] [ERROR] Could not create jsonl file: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+	}
+	defer jsonlFile.Close()
+
+	var fileQueue = make(chan string, 100)
+	var results = make(chan *FileInfo, 100)
+	var wg sync.WaitGroup
+	var mutex sync.Mutex // Mutex to ensure thread-safe writes to jsonlFile
+	var lineCount int    // To keep track of the number of JSON lines
+
+	numWorkers := runtime.NumCPU()
+	for i := 0; i < numWorkers; i++ {
+		go worker(fileQueue, results, logger, &wg)
+	}
+
+	go func() {
+		for result := range results {
+			jsonData, err := json.Marshal(result)
+			if err != nil {
+				logger.Printf("[%s] [ERROR] Error marshalling json for %s: %v\n", time.Now().UTC().Format(time.RFC3339), result.Path, err)
+			}
+
+			mutex.Lock()
+			_, err = jsonlFile.Write(jsonData)
+			if err != nil {
+				logger.Printf("[%s] [ERROR] Error writing to jsonl file: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+			}
+
+			_, err = jsonlFile.Write([]byte("\n"))
+			if err != nil {
+				logger.Printf("[%s] [ERROR] Error writing to jsonl file: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+			}
+			lineCount++
+			mutex.Unlock()
+		}
+	}()
+
+	err = filepath.Walk(`C:\`, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			logger.Printf("[%s] [ERROR] Error accessing path %s: %v\n", time.Now().UTC().Format(time.RFC3339), path, err)
+			return nil
+		}
+
+		if !info.IsDir() {
+			wg.Add(1)
+			fileQueue <- path
+		}
+		return nil
+	})
+	if err != nil {
+		logger.Printf("[%s] [ERROR] Error walking the path: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+	}
+
+	wg.Wait()
+	close(fileQueue)
+	close(results)
+
+	stopTime := time.Now().UTC()
+	totalDuration := stopTime.Sub(startTime)
+	logger.Printf("[%s] [INFO] Program started at: %s\n", stopTime.Format(time.RFC3339), startTimeStr)
+	logger.Printf("[%s] [INFO] Program stopped at: %s\n", stopTime.Format(time.RFC3339), stopTime.Format(time.RFC3339))
+	logger.Printf("[%s] [INFO] Total run time: %s\n", stopTime.Format(time.RFC3339), totalDuration)
+	logger.Printf("[%s] [INFO] Total JSON lines: %d\n", stopTime.Format(time.RFC3339), lineCount)
+
+	zipFileName := fmt.Sprintf("%s_%s.zip", hostname, strings.ReplaceAll(startTimeStr, ":", "-"))
+	zipFile, err := os.Create(zipFileName)
+	if err != nil {
+		log.Fatal("Could not create zip file:", err)
+	}
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer func() {
+		err1 := zipWriter.Close()
+		if err1 != nil {
+			log.Fatal("Could not close zip writer:", err1)
+		}
+
+		err2 := zipFile.Close()
+		if err2 != nil {
+			log.Fatal("Could not close zip file:", err2)
+		}
+
+		// Create and launch self-delete PowerShell script
+		exePath, err := os.Executable()
+		if err != nil {
+			logger.Printf("[%s] [ERROR] Could not get executable path: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+		} else {
+			scriptPath, err := createSelfDeleteScript(exePath)
+			if err != nil {
+				logger.Printf("[%s] [ERROR] Could not create self-delete script: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+			} else {
+				logger.Printf("[%s] [INFO] Self-delete script created: %s\n", time.Now().UTC().Format(time.RFC3339), scriptPath)
+				cmd := exec.Command("powershell", "-ExecutionPolicy", "Bypass", "-File", scriptPath)
+				err := cmd.Start()
+				if err != nil {
+					logger.Printf("[%s] [ERROR] Could not start self-delete script: %v\n", time.Now().UTC().Format(time.RFC3339), err)
+				}
+			}
+		}
+	}()
+
+	err = addFileToZip(zipWriter, logFileName)
+	if err != nil {
+		log.Fatal("Could not add log file to zip:", err)
+	}
+
+	err = addFileToZip(zipWriter, jsonlFileName)
+	if err != nil {
+		log.Fatal("Could not add jsonl file to zip:", err)
+	}
+}
